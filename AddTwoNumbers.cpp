a8bd3736de206ca38416ced2820f0c1d6ed03537
GodIsADick <bwilson0908@yahoo.com>
2025-08-13 12:22:35 -0700

    Add files via upload

---
+++ AddTwoNumbers.cpp
@@ -0,0 +1,25 @@
+// Created by Blake Wilson on 8/13/2025.
+// AddTwoNumbers.cpp
+// Create variables needed for this program.
+
+#include <iostream>
+using namespace std;
+
+// Create a very important function named main.
+// This is where your program starts.
+int main() {
+
+    // Create variables needed for this program.
+    int sumOFNums = 0;
+    int num1 = 0;
+    int num2 = 0;
+    cout << "Enter a whole number for num1: ";
+    cin >> num1;
+    cout << "Enter a whole number for num2: ";
+    cin >> num2;
+    sumOFNums = num1 + num2;
+    cout << "The sum of the two numbers is: " << sumOFNums;
+
+    return 0;
+}
+
